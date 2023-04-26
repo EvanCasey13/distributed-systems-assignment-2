43322ab062e9d4b75d5aaf502f54f40c492f90e3
EvanCasey13 <evancasey2003@gmail.com>
2023-04-26 12:09:19 +0100

    Added face percentage accuracy

--- livevideo.py
+++ livevideo.py
@@ -5,6 +5,7 @@ import cv2
 import numpy as np
 import face_recognition
 from threading import Thread
+import math
 
 app = Flask(__name__)
 socketioApp = SocketIO(app)
@@ -12,12 +13,12 @@ socketioApp = SocketIO(app)
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("obama.jpg")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+dustin_image = face_recognition.load_image_file("dustin_p.png")
+dustin_face_encoding = face_recognition.face_encodings(dustin_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("biden.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+alex_image = face_recognition.load_image_file("alex_pereira.png")
+alex_face_encoding = face_recognition.face_encodings(alex_image)[0]
 
 # Load a third sample picture and learn how to recognize it.
 evan_image = face_recognition.load_image_file("evan.jpg")
@@ -25,13 +26,13 @@ evan_face_encoding = face_recognition.face_encodings(evan_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
-    obama_face_encoding,
-    biden_face_encoding,
+    dustin_face_encoding,
+    alex_face_encoding,
     evan_face_encoding
 ]
 known_face_names = [
-    "Barack Obama",
-    "Joe Biden",
+    "Dustin Porier",
+    "Alex Pereira",
     "Evan Casey"
 ]
 
@@ -40,6 +41,16 @@ face_locations = []
 face_encodings = []
 face_names = []
 
+def face_acc_percent(face_distance, face_match_threshold=0.6):
+       range = (1.0 - face_match_threshold)
+       linear_val = (1.0 - face_distance) / (range * 2.0)
+
+       if face_distance > face_match_threshold:
+            return str(round(linear_val * 100, 2)) + '%'
+       else:
+          value = (linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))) * 100
+       return str(round(value, 2)) + '%'
+
 def face_model(img):
         process_this_frame = True
  # Only process every other frame of video to save time
@@ -47,26 +58,27 @@ def face_model(img):
 
          # Resize frame of video to 1/4 size for faster face recognition processing
          small_frame = cv2.resize(img, (0, 0), fx=0.25, fy=0.25)
-        img_h, img_w = img.shape[:2]
-        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
         ##print(len(faces)) ##Check amount of faces on screen
         #for every face found:
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(small_frame)
         face_encodings = face_recognition.face_encodings(small_frame, face_locations)
+
         face_names = []
         for face_encoding in face_encodings:
                 # See if the face is a match for the known face(s)
                 matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
                 name = "Unknown"
+                confidence = "?"
                 # Or instead, use the known face with the smallest distance to the new face
                 face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
                 best_match_index = np.argmin(face_distances)
                 if matches[best_match_index]:
                     name = known_face_names[best_match_index]
+                    confidence = face_acc_percent(face_distances[best_match_index], face_match_threshold=0.6)
 
-                face_names.append(name)
+                face_names.append(name + " " + confidence)
 
                 process_this_frame = not process_this_frame
             
@@ -82,7 +94,6 @@ def face_model(img):
             font = cv2.FONT_HERSHEY_DUPLEX
             cv2.putText(img, name, (h + 5 , x - 35), font, 1.0, (255, 255, 255), 1)
 
-            cv2.putText(img, "Match 100%", (h, x - 10), font, 1.0, (255, 255, 255), 1)
             break
 
 def gen_frames():
